72211cdd60263304e629945c9a9ebec693907e35
Angellomon <angelmtzdiaz@gmail.com>
2023-10-01 16:33:21 -0600

    feat(routes/mkt): impl main page

--- src/routes/mkt/+page.svelte
+++ src/routes/mkt/+page.svelte
@@ -1,1 +1,42 @@
-<h1>MKT</h1>
+<script>
+	import ActivityButton from '$lib/activity-button.svelte';
+
+	let width = '40vw';
+</script>
+
+<section>
+	<div class="message">What do you want to plan today?</div>
+
+	<div class="activity-select">
+		<ActivityButton path="/mkt/materials-deployment" {width}>Materials Deployment</ActivityButton>
+
+		<ActivityButton path="/mkt/material-quantity" {width}
+			>Material Quantity for Next Cycle</ActivityButton
+		>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+
+		justify-content: space-around;
+		align-items: center;
+
+		/* width: 100vw; */
+	}
+
+	div.message {
+		text-align: center;
+
+		font-size: 24px;
+	}
+
+	div.activity-select {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	}
+</style>
