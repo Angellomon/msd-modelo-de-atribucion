45b011fcc42794a8eed7113b76f58ea0e81e73ea
Angellomon <angelmtzdiaz@gmail.com>
2023-10-01 12:15:57 -0600

    feat(routes): impl /mkt & /sales

---
+++ src/routes/sales/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import ActivityButton from '$lib/activity-button.svelte';
+</script>
+
+<section>
+	<div class="message">What do you want to plan today?</div>
+
+	<div class="activity-select">
+		<ActivityButton path="/sales/visit-frequency">Visit Frequency</ActivityButton>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+
+		justify-content: center;
+		align-items: center;
+	}
+
+	div.message {
+		width: 50vw;
+		text-align: center;
+	}
+
+	div.activity-select {
+		width: 50vw;
+	}
+</style>
