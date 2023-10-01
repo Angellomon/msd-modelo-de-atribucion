8be7791f30bc4a76a3267c0ef277aedd876af4b1
Angellomon <angelmtzdiaz@gmail.com>
2023-10-01 12:16:20 -0600

    feat(routes): impl index

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import './styles.css';
 </script>
 
-<slot />
+<div>
+	<nav>
+		<h1><u>Consumer Engagement Planning Assistance</u></h1>
+		<h3><i>Orchestrate multichannel trough this personal assistant for engagement planning</i></h3>
+	</nav>
+	<slot />
+</div>
+
+<style>
+	nav {
+		padding: 1vh 3vw;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,28 @@
-<h1>Sitio: Modelos de Atribución</h1>
+<script>
+	import ActivityButton from '$lib/activity-button.svelte';
+</script>
+
+<main>
+	<div class="main-buttons">
+		<ActivityButton path="/mkt">MKT</ActivityButton>
+		<ActivityButton path="/sales">SALES</ActivityButton>
+	</div>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+	}
+
+	div.main-buttons {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 20px;
+
+		justify-content: center;
+		align-items: center;
+
+		width: 100%;
+	}
+</style>
