6cb2ea38e7fcc163bddc9b3aed0a28ed75e34720
Angellomon <angelmtzdiaz@gmail.com>
2023-10-02 12:24:07 -0600

    chore: simplification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,4 @@
 	import './styles.css';
 </script>
 
-<div>
-	<nav>
-		<h1><u>Consumer Engagement Planning Assistance</u></h1>
-		<h3><i>Orchestrate multichannel trough this personal assistant for engagement planning</i></h3>
-	</nav>
-	<slot />
-</div>
-
-<style>
-	nav {
-		padding: 1vh 3vw;
-	}
-</style>
+<slot />
