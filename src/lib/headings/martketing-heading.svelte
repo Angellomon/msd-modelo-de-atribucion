42c0652c0a6faa69dbef70ddbc8961c6b78ed5cb
Angellomon <angelmtzdiaz@gmail.com>
2023-10-02 14:01:20 -0600

    feat(lib/headings/marketing): impl heading

---
+++ src/lib/headings/martketing-heading.svelte
@@ -0,0 +1,57 @@
+<div class="heading">
+	<h1>Consumer Engagement Planning Assistance</h1>
+	<p>
+		Orchestrate <b>multichannel</b> trough this personal assistant for engagement <b>planning</b>
+	</p>
+
+	<div class="message">
+		Who do you want to
+		<b>plan today?</b>
+	</div>
+</div>
+
+<style>
+	h1 {
+		background-color: white;
+		color: var(--verde-msd);
+
+		border-radius: 200px;
+
+		vertical-align: middle;
+		padding: 3vh;
+		line-height: 2rem;
+
+		font-size: 2rem;
+
+		font-family: 'Invention Bold';
+
+		padding-left: 20vh;
+		margin-left: -20vh;
+	}
+
+	div.heading {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+	}
+
+	div.message {
+		background-color: white;
+		color: var(--verde-msd);
+
+		margin-bottom: 10vh;
+		font-size: 2rem;
+
+		padding: 20px 15px;
+	}
+
+	p {
+		font-size: 2rem;
+		width: 70%;
+
+		padding: 0;
+		margin: 0;
+		margin-bottom: 5vh;
+	}
+</style>
