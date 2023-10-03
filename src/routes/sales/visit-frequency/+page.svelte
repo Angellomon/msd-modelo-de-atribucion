b8d0088a24c1f3704ff503cc7aec47db0cd9491d
Angellomon <angelmtzdiaz@gmail.com>
2023-10-03 17:34:06 -0600

    feat: bg

--- src/routes/sales/visit-frequency/+page.svelte
+++ src/routes/sales/visit-frequency/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Visit Frequency</h1>
+<section>
+	<h1>Visit Frequency</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+
+		position: relative;
+
+		justify-content: flex-start;
+		align-items: flex-start;
+
+		/* padding-left: 10vh;
+		padding-top: 5vh; */
+
+		background-color: var(--verde-msd);
+		height: 100vh;
+	}
+</style>
