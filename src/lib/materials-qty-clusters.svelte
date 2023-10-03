ace613577f66c10366a22b4c3020f9f268aafbb9
Angellomon <angelmtzdiaz@gmail.com>
2023-10-03 00:25:08 -0600

    feat(lib/materials-qty-clusters): impl material quantity clusters component

---
+++ src/lib/materials-qty-clusters.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { MaterialsQtyCluster } from "$lib/data";
+	export let cluster: MaterialsQtyCluster;
+</script>
+
+<div class="cluster">
+	<h1>{cluster.channel.name}</h1>
+
+	{#each cluster.materials as material}
+		<span>{material.name}</span>
+	{/each}
+</div>
+
+<style>
+	div.cluster {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+
+		/* width: 20vw; */
+	}
+</style>
