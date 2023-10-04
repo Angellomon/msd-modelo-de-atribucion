b66dfd43c2a30e0489fd72caa597e55c4993b52c
Angellomon <angelmtzdiaz@gmail.com>
2023-10-04 11:19:55 -0600

    feat(mkt/material-qty): impl interactivity

--- src/lib/materials-qty-clusters.svelte
+++ src/lib/materials-qty-clusters.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import type { MaterialsQtyCluster } from "$lib/data";
+	import { selectedFranchisesKeys } from "$lib/store";
 	export let cluster: MaterialsQtyCluster;
 </script>
 
 <div class="cluster">
 	<h1>{cluster.channel.name}</h1>
 
-	{#each cluster.materials as material}
+	{#each cluster.materials.filter( (m) => $selectedFranchisesKeys.includes(m.franchiseKey), ) as material}
 		<span>{material.name}</span>
 	{/each}
 </div>
