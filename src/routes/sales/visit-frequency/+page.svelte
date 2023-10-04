7ae3189debc28b2e26177875ed1987de9584de2d
Angellomon <angelmtzdiaz@gmail.com>
2023-10-04 03:16:29 -0600

    feat(sales/visit-frequency): impl charts

--- src/routes/sales/visit-frequency/+page.svelte
+++ src/routes/sales/visit-frequency/+page.svelte
@@ -26,6 +26,25 @@
 		{
 			key: 'current-high-interaction',
 			name: 'High Interaction',
+			label: 'current',
+			historicData: [
+				{
+					impacts: 20,
+					month: 'Enero'
+				},
+				{
+					impacts: 15,
+					month: 'Febrero'
+				},
+				{
+					impacts: 25,
+					month: 'Marzo'
+				},
+				{
+					impacts: 35,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -52,6 +71,25 @@
 		{
 			key: 'current-medium-interaction',
 			name: 'Medium Interaction',
+			label: 'current',
+			historicData: [
+				{
+					impacts: 20,
+					month: 'Enero'
+				},
+				{
+					impacts: 15,
+					month: 'Febrero'
+				},
+				{
+					impacts: 25,
+					month: 'Marzo'
+				},
+				{
+					impacts: 35,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -78,6 +116,25 @@
 		{
 			key: 'current-low-interaction',
 			name: 'Low Interaction',
+			label: 'current',
+			historicData: [
+				{
+					impacts: 20,
+					month: 'Enero'
+				},
+				{
+					impacts: 15,
+					month: 'Febrero'
+				},
+				{
+					impacts: 25,
+					month: 'Marzo'
+				},
+				{
+					impacts: 35,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -107,6 +164,25 @@
 		{
 			key: 'suggested-high-interaction',
 			name: 'High Interaction',
+			label: 'Suggested',
+			historicData: [
+				{
+					impacts: 15,
+					month: 'Enero'
+				},
+				{
+					impacts: 30,
+					month: 'Febrero'
+				},
+				{
+					impacts: 25,
+					month: 'Marzo'
+				},
+				{
+					impacts: 25,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -133,6 +209,25 @@
 		{
 			key: 'suggested-medium-interaction',
 			name: 'Medium Interaction',
+			label: 'Suggested',
+			historicData: [
+				{
+					impacts: 25,
+					month: 'Enero'
+				},
+				{
+					impacts: 35,
+					month: 'Febrero'
+				},
+				{
+					impacts: 30,
+					month: 'Marzo'
+				},
+				{
+					impacts: 40,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -159,6 +254,25 @@
 		{
 			key: 'suggested-low-interaction',
 			name: 'Low Interaction',
+			label: 'Suggested',
+			historicData: [
+				{
+					impacts: 10,
+					month: 'Enero'
+				},
+				{
+					impacts: 15,
+					month: 'Febrero'
+				},
+				{
+					impacts: 33,
+					month: 'Marzo'
+				},
+				{
+					impacts: 35,
+					month: 'Abril'
+				}
+			],
 			channels: [
 				{
 					key: 'f2f-visits',
@@ -206,8 +320,15 @@
 
 		<div class="clusters">
 			<div class="current-clusters">
-				{#each currentClusters as cluster}
-					<VisitFrequencyClusterComponent {cluster} impacts={calculateImpacts(cluster)} />
+				{#each currentClusters as cluster, i}
+					<VisitFrequencyClusterComponent
+						{cluster}
+						impacts={calculateImpacts(cluster)}
+						chartData={{
+							current: currentClusters[i].historicData,
+							suggested: suggestedClusters[i].historicData
+						}}
+					/>
 				{/each}
 			</div>
 
@@ -218,6 +339,8 @@
 			</div>
 		</div>
 	</div>
+
+	<img src="/img/SALES-REP-CIRCULO.png" alt="SALES" />
 </section>
 
 <style>
@@ -324,5 +447,13 @@
 		display: flex;
 		flex-direction: column;
 		gap: 40px;
+	}
+
+	img {
+		position: absolute;
+		top: 25px;
+		right: 25px;
+
+		width: 20vh;
 	}
 </style>
